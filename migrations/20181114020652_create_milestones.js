d9c5aa164b781c705aea644a8f29fbd47a59a04a
Ying Dong <dongyingname@yahoo.com>
2018-11-13 19:44:25 -0700

    milestones migration file: has functional migrantion up and rollback. Auto increment is added as addtional column.

--- migrations/20181114020652_create_milestones.js
+++ migrations/20181114020652_create_milestones.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function(knex, Promise) {  
+    return Promise.all([
+      knex.schema.createTable('milestones', function(table){
+        table.string('project_name');
+        table.string('rating');
+        table.timestamps();
+        table.increments('id');
+      })
+    ]);
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex, Promise) {  
+    return Promise.all([
+      knex.schema.dropTable('milestones')
+    ]);
 };
